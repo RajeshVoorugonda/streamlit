af06d9a1c871ebc93b279dbae3e7b92ff9b2b0a7
Voorugonda Rajesh <76484557+RajeshVoorugonda@users.noreply.github.com>
2023-04-26 01:53:21 +0530

    updated files add

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import streamlit as st
 import io
 from contextlib import redirect_stdout
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from keras.models import load_model
-import os
+
+#Install required packages for streamlit
+#import os
 
 # Install packages listed in requirements.txt
 #os.system('pip install -r requirements.txt')
+
 
 def load_saved(file_name):
     df = pd.read_csv(f'{file_name}')
@@ -24,8 +27,7 @@ train_accuracy=load_saved("train_accuracy.csv")
 
 n_layers = st.slider('Number of Convolutional Layers', 1, 5, 1)
 
-model=load_model(f'model_{n_layers}.h5')
-st.write('Training Time:', times[n_layers-1])
+model=load_model(f'model{n_layers}.h5')
 st.write('Test Accuracy:', test_accuracy[n_layers-1])
 st.write('Train Accuracy:', train_accuracy[n_layers-1])
 
@@ -35,6 +37,8 @@ st.write('Number of Parameters:', no_of_params)
 
 
 st.write('Training loss: ', history[n_layers-1])
+
+st.write('Training Time:', times[n_layers-1])
 
 
 
@@ -76,17 +80,21 @@ st.header("Plots")
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))
 k=[1,2,3,4,5]
-axs[0,0].plot(k,times)
-axs[0,0].set_ylim(0, 5000)
-axs[0,0].set_title('Training Time Vs No.of layers')
-axs[0,1].plot(k,history)
-axs[0,1].set_ylim(0, 1)
-axs[0,1].set_title('Training loss Vs No.of layers')
-axs[1,0].plot(k,test_accuracy)
-axs[1,0].set_ylim(0.5, 1)
-axs[1,0].set_title('Test accuracy Vs No.of layers')
-axs[1,1].plot(k,train_accuracy)
-axs[1,1].set_ylim(0.5, 1)
-axs[1,1].set_title('Training accuracy Vs No.of layers')
+axs[1,0].plot(k,history)
+axs[1,0].set_ylim(0.5, 1.5)
+axs[1,0].set_title('Training loss Vs No.of layers')
+axs[0,0].plot(k,test_accuracy)
+axs[0,0].set_ylim(0.6, 0.8)
+axs[0,0].set_title('Test accuracy Vs No.of layers')
+axs[0,1].plot(k,train_accuracy)
+axs[0,1].set_ylim(0.6, 0.8)
+axs[0,1].set_title('Training accuracy Vs No.of layers')
+axs[1,1].plot(k,times)
+axs[1,1].set_ylim(3000, 5000)
+axs[1,1].set_title('Training Time Vs No.of layers')
 
 st.pyplot(fig)
+
+
+
+
